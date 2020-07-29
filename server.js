2fc4654af7eca18362650c09ce62493e4df32f3b
Arkadiusz Smagała <asmagala@outlook.com>
2020-07-29 23:09:18 +0200

    Build the project, config it to be used in Heroku.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
+const path = require('path');
+
 
 const app = express();
 const cors = require('cors');
@@ -12,15 +14,20 @@ const seatsRoutes = require('./routes/seats.routes');
 app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 app.use((req, res) => {
   res.status(404).json({ "message": "Not found..."});
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
